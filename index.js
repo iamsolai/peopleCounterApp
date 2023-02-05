cbee8e2157f3f97501185cdaeb6898e64165610d
Solai Mithelesh <86795893+iamsolai@users.noreply.github.com>
2023-02-05 14:59:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+
+function increment() {
+  count++;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " -> ";
+  saveEl.textContent = saveEl.textContent + " " + countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
